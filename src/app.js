cfdaa8205f7f8acfb977c0505aeb29f192869c45
hiepnxph <hiepnxph@gmail.com>
2022-07-25 09:09:31 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+$('.post').slick({
+        autoplay:true,
+        autoplaySpeed:1500,
+        arrows:true,
+        centerMode:true,
+        slidesToShow:5,
+        slidesToScroll:2,
+        arrows:false,
+    });
+    
